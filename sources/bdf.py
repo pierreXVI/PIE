e92dc826c69859693538ca2af903a20f99a54f2c
Pierre Seize <pierreseize@gmail.com>
2018-10-16 20:15:59 +0200

    Cleaning after a training day Rewriting all methods to fit the same convention Writing cleaner tests Adapting the BDF methods for multidimensional array

--- sources/bdf.py
+++ sources/bdf.py
@@ -1,13 +1,14 @@
 import numpy as np
 import scipy.optimize
-from rk import rk_4, Euler_explicit
+from rk import rk_4
 
 
-def bdf_2(y0, t, f):
+def bdf_1(y0, t, f):
     """
-    Gear method 2nd order
+    BDF1 or Implicit Euler method
     y' = f(y, t)
     y(t[0]) = y0
+
     :param y0: initial value, may be multi-dimensional of size d
     :param t: array of time steps, of size n
     :param f: a function with well shaped input and output
@@ -19,78 +20,137 @@ def bdf_2(y0, t, f):
     except TypeError:
         n = len(t)
         y = np.zeros((n,))
-
     y[0] = y0
-    y[1] = y0 + (t[1] - t[0]) * f(y0, t[0])
+    for i in range(n - 1):
+        def func_to_minimise(u):
+            return u - y[i] - (t[i + 1] - t[i]) * f(u, t[i + 1])
 
-    for i in range(1, n - 1):
-        def g(u):
-            return u - (4. / 3. * y[i] - 1. / 3. * y[i - 1] + 2. / 3. * (t[i + 1] - t[i]) * f(u, t[i + 1]))
-
-        y[i + 1] = scipy.optimize.newton(g, y[i])
-
+        y[i + 1] = scipy.optimize.root(func_to_minimise, y[i]).x
     return y
 
 
-import scipy as sc
-import math as m
-from scipy.optimize import newton
+def bdf_2(y0, t, f):
+    """
+    BDF2 method
+    y' = f(y, t)
+    y(t[0]) = y0
+
+    :param y0: initial value, may be multi-dimensional of size d
+    :param t: array of time steps, of size n
+    :param f: a function with well shaped input and output
+    :return: the solution, of shape (n, d)
+    """
+    try:
+        n, d = len(t), len(y0)
+        y = np.zeros((n, d))
+    except TypeError:
+        n = len(t)
+        y = np.zeros((n,))
+    y[:2] = rk_4(y0, t[:2], f)
+    for i in range(n - 2):
+        def func_to_minimise(u):
+            return u - 4. * y[i + 1] / 3. + y[i] / 3. - 2. * (t[i + 2] - t[i + 1]) * f(u, t[i + 2]) / 3.
+
+        y[i + 2] = scipy.optimize.root(func_to_minimise, y[i + 1]).x
+    return y
 
 
-def real(t):
-    return np.exp(t / 2 - np.sin(2 * t) / 4)
+def bdf_3(y0, t, f):
+    """
+    BDF3 method
+    y' = f(y, t)
+    y(t[0]) = y0
+
+    :param y0: initial value, may be multi-dimensional of size d
+    :param t: array of time steps, of size n
+    :param f: a function with well shaped input and output
+    :return: the solution, of shape (n, d)
+    """
+    try:
+        n, d = len(t), len(y0)
+        y = np.zeros((n, d))
+    except TypeError:
+        n = len(t)
+        y = np.zeros((n,))
+    y[:3] = rk_4(y0, t[:3], f)
+    for i in range(n - 3):
+        def func_to_minimise(u):
+            return u \
+                   - 18. * y[i + 2] / 11. \
+                   + 9. * y[i + 1] / 11. \
+                   - 2. * y[i] / 11. \
+                   - 6 * (t[i + 3] - t[i + 2]) * f(u, t[i + 3]) / 11.
+
+        y[i + 3] = scipy.optimize.root(func_to_minimise, y[i + 2]).x
+    return y
 
 
-def fGear(y, sol, tStep, t, function):
-    return (18. / 11.) * sol[-1] - (9. / 11.) * sol[-2] + (2. / 11.) * sol[-3] + (6. / 11.) * tStep * function(y, t) - y
+def bdf_4(y0, t, f):
+    """
+    BDF4 method
+    y' = f(y, t)
+    y(t[0]) = y0
+
+    :param y0: initial value, may be multi-dimensional of size d
+    :param t: array of time steps, of size n
+    :param f: a function with well shaped input and output
+    :return: the solution, of shape (n, d)
+    """
+    try:
+        n, d = len(t), len(y0)
+        y = np.zeros((n, d))
+    except TypeError:
+        n = len(t)
+        y = np.zeros((n,))
+    y[:4] = rk_4(y0, t[:4], f)
+    for i in range(n - 4):
+        def func_to_minimise(u):
+            return u \
+                   - 48. * y[i + 3] / 25. \
+                   + 36. * y[i + 2] / 25. \
+                   - 16. * y[i + 1] / 25. \
+                   + 3. * y[i] / 25. \
+                   - 12 * (t[i + 4] - t[i + 3]) * f(u, t[i + 4]) / 25.
+
+        y[i + 4] = scipy.optimize.root(func_to_minimise, y[i + 3]).x
+    return y
 
 
-# Calcul des coefs pour RK
-def coefRK(ord, f, ti, yi):
-    if ord == 1:
-        k1 = f(ti, yi)
-        return k1
+def bdf_5(y0, t, f):
+    """
+    BDF5 method
+    y' = f(y, t)
+    y(t[0]) = y0
 
+    :param y0: initial value, may be multi-dimensional of size d
+    :param t: array of time steps, of size n
+    :param f: a function with well shaped input and output
+    :return: the solution, of shape (n, d)
+    """
+    try:
+        n, d = len(t), len(y0)
+        y = np.zeros((n, d))
+    except TypeError:
+        n = len(t)
+        y = np.zeros((n,))
+    y[:5] = rk_4(y0, t[:5], f)
+    for i in range(n - 5):
+        def func_to_minimise(u):
+            return u \
+                   - 300. * y[i + 4] / 137. \
+                   + 300. * y[i + 3] / 137. \
+                   - 200. * y[i + 2] / 137. \
+                   + 75. * y[i + 1] / 137. \
+                   - 12. * y[i] / 137. \
+                   - 60 * (t[i + 5] - t[i + 4]) * f(u, t[i + 5]) / 137.
 
-def bdf_3(f, y0, t0, tEnd, tStep):
-    temps = [t0]
-    sol = [y0]
-    t = t0
-    yact = y0
-    for i in range(2):
-        # Premieres iterations calculees avec RK
-        yact = yact + coefRK(1, f, t, yact) * tStep
-        t += tStep
-        sol.append(yact)
-        temps.append(t)
-    while t < tEnd:
-        t += tStep
-        yact = newton(fGear, sol[-1], args=(sol, tStep, t, f))
-        sol.append(yact)
-        temps.append(t)
-    return sol
+        y[i + 5] = scipy.optimize.root(func_to_minimise, y[i + 4]).x
+    return y
 
-
-def bdf_4(y0, t0, tf, n, f):
-    t = t0
-    y = y0
-    h = (tf - t0) / float(n)
-    Approx_solution = Euler_explicit(f, t0, t0 + 3 * h, y0, 3)[0]
-    Temps = [t0 + i*h for i in range(4)]
-    for i in range(n-4):
-        t += h
-        Temps.append(t)
-
-        def F1(z):
-            return z - (48. / 25) * Approx_solution[i + 3] + (36. / 25) * Approx_solution[i + 2] - (16. / 25) * \
-                   Approx_solution[i + 1] + (3. / 25) * Approx_solution[i] - (12. / 25) * h * f(z, Temps[i + 3])
-
-        Approx_solution.append(scipy.optimize.newton(F1, Approx_solution[i + 3]))
-    return Approx_solution
 
 def bdf_6(y0, t, f):
     """
-    Backward Differentiation Formula - 6 method
+    BDF6 method
     y' = f(y, t)
     y(t[0]) = y0
 
@@ -107,13 +167,17 @@ def bdf_6(y0, t, f):
         y = np.zeros((n,))
     y[:6] = rk_4(y0, t[:6], f)
     for i in range(n - 6):
-        h = t[i + 1] - t[i]
-
         def func_to_minimise(u):
-            return u - 360. * y[i + 5] / 147. + 450. * y[i + 4] / 147. - 400. * y[i + 3] / 147. + 225. * y[
-                i + 2] / 147. - 72. * y[i + 1] / 147. + 10. * y[i] / 147. - 60. * h * f(u, t[i + 6]) / 147.
+            return u \
+                   - 360. * y[i + 5] / 147. \
+                   + 450. * y[i + 4] / 147. \
+                   - 400. * y[i + 3] / 147. \
+                   + 225. * y[i + 2] / 147. \
+                   - 72. * y[i + 1] / 147. \
+                   + 10. * y[i] / 147. \
+                   - 60 * (t[i + 6] - t[i + 5]) * f(u, t[i + 6]) / 147.
 
-        y[i + 6] = scipy.optimize.newton(func_to_minimise, y[i + 5])
+        y[i + 6] = scipy.optimize.root(func_to_minimise, y[i + 5]).x
     return y
 
 
--- sources/test.py
+++ sources/test.py
@@ -4,11 +4,17 @@ import rk
 import bdf
 
 
+def f(y, t):
+    return y * (np.sin(t) ** 2)
+
+
+def solution(t):
+    return np.exp(t / 2 - np.sin(2 * t) / 4)
+
+
 def compare_methods_rk():
     """
-    Compare RK1, RK2 and RK4 methods on:
-    y' = y * sin(t)**2
-    y(0) = 1
+    Compare RK1, RK2 and RK4 methods
     """
     fig = plt.figure()
     ax1 = fig.add_subplot(211)
@@ -19,10 +25,7 @@ def compare_methods_rk():
     x = np.linspace(0, 10, 100)
     y0 = 1
 
-    def f(y, t):
-        return y * (np.sin(t) ** 2)
-
-    y_exact = np.exp(x / 2 - np.sin(2 * x) / 4)
+    y_exact = solution(x)
     y1 = rk.rk_1(y0, x, f)
     y2 = rk.rk_2(y0, x, f)
     y4 = rk.rk_4(y0, x, f)
@@ -40,9 +43,68 @@ def compare_methods_rk():
     plt.show()
 
 
+def compare_methods_bdf():
+    """
+    Compare BDF1-6 methods
+    """
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    ax1.grid(True)
+    ax1.set_ylabel(r'$y$')
+    ax2 = fig.add_subplot(212, sharex=ax1)
+    ax2.grid(True)
+    ax2.set_xlabel(r'$x$')
+    ax2.set_ylabel(r'$\left|y - y_{exact}\right|$')
+
+    x = np.linspace(0, 10, 100)
+    y0 = 1
+
+    y_exact = solution(x)
+    ax1.plot(x, y_exact, c='k', label=r'$y_{exact}$')
+
+    for method, label in zip((bdf.bdf_1, bdf.bdf_2, bdf.bdf_3, bdf.bdf_4, bdf.bdf_5, bdf.bdf_6),
+                             ('BDF1', 'BDF2', 'BDF3', 'BDF4', 'BDF5', 'BDF6')):
+        y = method(y0, x, f)
+        color = ax1.plot(x, y, '+', label=label)[0].get_color()
+        ax2.semilogy(x, abs(y - y_exact), c=color)
+
+    ax1.legend()
+    plt.show()
+
+
+def compare_all_methods():
+    """
+    Compare all methods
+    """
+    fig = plt.figure()
+    ax1 = fig.add_subplot(211)
+    ax1.grid(True)
+    ax1.set_ylabel(r'$y$')
+    ax2 = fig.add_subplot(212, sharex=ax1)
+    ax2.grid(True)
+    ax2.set_xlabel(r'$x$')
+    ax2.set_ylabel(r'$\left|y - y_{exact}\right|$')
+
+    x = np.linspace(0, 10, 100)
+    y0 = 1
+
+    y_exact = solution(x)
+    ax1.plot(x, y_exact, c='k', label=r'$y_{exact}$')
+
+    for method, label in zip(
+            (rk.rk_1, rk.rk_2, rk.rk_4, bdf.bdf_1, bdf.bdf_2, bdf.bdf_3, bdf.bdf_4, bdf.bdf_5, bdf.bdf_6),
+            ('RK1', 'RK2', 'RK4', 'BDF1', 'BDF2', 'BDF3', 'BDF4', 'BDF5', 'BDF6')):
+        y = method(y0, x, f)
+        color = ax1.plot(x, y, '+', label=label)[0].get_color()
+        ax2.semilogy(x, abs(y - y_exact), c=color)
+
+    ax1.legend()
+    plt.show()
+
+
 def compare_methods_2d():
     """
-    Compare RK1, RK2 and RK4 methods on a harmonic oscillator problem:
+    Compare methods on a harmonic oscillator problem:
     y'' + y = 0
     y(0) = 1
     y'(0) = 0
@@ -50,66 +112,38 @@ def compare_methods_2d():
     fig = plt.figure()
     ax1 = fig.add_subplot(211)
     ax1.grid(True)
+    ax1.set_ylabel(r'$y$')
     ax2 = fig.add_subplot(212, sharex=ax1)
     ax2.grid(True)
+    ax2.set_xlabel(r'$x$')
+    ax2.set_ylabel(r'$\left|y - y_{exact}\right|$')
 
     x = np.linspace(0, 10 * np.pi, 1000)
     y0 = np.array([1, 0])
 
-    def f(y, t):
+    def f_harmonic(y, t):
         return np.array([y[1], -y[0]])
 
     y_exact = np.cos(x)
-    y1 = rk.rk_1(y0, x, f)[:, 0]
-    y2 = rk.rk_2(y0, x, f)[:, 0]
-    y4 = rk.rk_4(y0, x, f)[:, 0]
+    ax1.plot(x, y_exact, c='k', label=r'$y_{exact}$')
 
-    ax1.plot(x, y1, '+', label='RK1')
-    ax1.plot(x, y2, '+', label='RK2')
-    ax1.plot(x, y4, '+', label='RK4')
-    ax1.plot(x, y_exact, label='Exact')
+    score = dict()
+    for method, label in zip(
+            (rk.rk_1, rk.rk_2, rk.rk_4, bdf.bdf_1, bdf.bdf_2, bdf.bdf_3, bdf.bdf_4, bdf.bdf_5, bdf.bdf_6),
+            ('RK1', 'RK2', 'RK4', 'BDF1', 'BDF2', 'BDF3', 'BDF4', 'BDF5', 'BDF6')):
+        y = method(y0, x, f_harmonic)
+        color = ax1.plot(x, y[:, 0], '+', label=label)[0].get_color()
+        ax2.semilogy(x, abs(y[:, 0] - y_exact), c=color)
+        score[label] = np.sum(abs(y[:, 0] - y_exact))
 
-    ax2.plot(x, abs(y1 - y_exact))
-    ax2.plot(x, abs(y2 - y_exact))
-    ax2.plot(x, abs(y4 - y_exact))
-
-    ax1.legend()
-    plt.show()
-
-
-def compare_methods_bdf():
-    fig = plt.figure()
-    ax1 = fig.add_subplot(211)
-    ax1.grid(True)
-    ax2 = fig.add_subplot(212, sharex=ax1)
-    ax2.grid(True)
-
-    x = np.linspace(0, 10, 100)
-    y0 = 1
-
-    def f(y, t):
-        return y * (np.sin(t) ** 2)
-
-    y_exact = np.exp(x / 2 - np.sin(2 * x) / 4)
-    y2 = bdf.bdf_2(y0, x, f)
-    y3 = bdf.bdf_3(f, y0, x[0], x[-1], (x[-1] - x[0])/len(x))
-    y4 = bdf.bdf_4(y0, x[0], x[-1], len(x), f)
-    y6 = bdf.bdf_6(y0, x, f)
-
-    ax1.plot(x, y2, '+', label='BDF2')
-    ax1.plot(x, y3[:100], '+', label='BDF3')
-    ax1.plot(x, y4, '+', label='BDF4')
-    ax1.plot(x, y6, '+', label='BDF6')
-    ax1.plot(x, y_exact, label='Exact')
-
-    ax2.plot(x, abs(y2 - y_exact))
-    ax2.plot(x, abs(y3[:100] - y_exact))
-    ax2.plot(x, abs(y4 - y_exact))
-    ax2.plot(x, abs(y6 - y_exact))
+    print(sorted(score, key=score.__getitem__))
 
     ax1.legend()
     plt.show()
 
 
 if __name__ == '__main__':
-    compare_methods_bdf()
+    # compare_methods_rk()
+    # compare_methods_bdf()
+    # compare_all_methods()
+    compare_methods_2d()
