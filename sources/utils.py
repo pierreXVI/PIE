5b92af4e6003ea260eb71769eb1a4f4bde94448d
Pierre Seize <pierreseize@gmail.com>
2018-11-02 23:20:49 +0100

    Moving jacobian + creating test for it

--- sources/utils.py
+++ sources/utils.py
@@ -2,7 +2,16 @@ import numpy as np
 import scipy.linalg
 
 
-def phi_1(z):
+def err(foo, bar, eps=1E-12):
+    if abs(bar) < eps:
+        return abs(foo)
+    return 100 * abs(1 - foo / bar)
+
+
+err = np.vectorize(err)
+
+
+def phi_1(z, eps=1E-12):
     """
     The `phi_1` function as defined in "A short course on exponential integrators" by Marlis Hochbruck
     phi_1(z) = \int_0^1 \e^{\left(1 - s\right)z}\mathrm{d}s
@@ -12,13 +21,14 @@ def phi_1(z):
     Else, phi_1(z) = \sum_{n \gt 0} z^n/(n + 1)!
 
     :param z: array_like
+    :param eps: Convergence criteria used by to approximate the function
     :return: phi_1(z)
     """
     if type(z) == np.ndarray:
         try:
             return np.dot(scipy.linalg.expm(z) - np.eye(z.shape[0]), np.linalg.inv(z))
         except np.linalg.LinAlgError:
-            return _phi_1_approx(z)
+            return _phi_1_approx(z, eps=eps)
     return (np.exp(z) - 1) / z
 
 
@@ -36,13 +46,6 @@ def _phi_1_approx(z, eps=1E-12):
 
 
 def test_phi_1(eps=1E-10):
-    def err(foo, bar):
-        if abs(bar) < eps:
-            return abs(foo)
-        return 100 * abs(1 - foo / bar)
-
-    err = np.vectorize(err)
-
     phi1_1 = 1.718281828459045235360287471352662497757247093699959574966
     phi1_10 = 2202.546579480671651695790064528424436635351261855678107423
     exp_10 = 22026.46579480671651695790064528424436635351261855678107423
@@ -94,5 +97,33 @@ def test_phi_1(eps=1E-10):
     print('\t' + flag * 'Success' + (not flag) * 'Failure')
 
 
+def jacobian(f, u, t, eps=1E-5):
+    try:
+        d = len(u)
+        jac = np.zeros((d, d))
+        for j in range(d):
+            e_j = np.zeros(d)
+            e_j[j] = eps
+            jac[:, j] = f(u + e_j, t) - f(u - e_j, t)
+    except TypeError:
+        jac = f(u + eps, t) - f(u - eps, t)
+    return jac / (2 * eps)
+
+
+def test_jacobian():
+    a, y, t = np.random.random(), np.random.random(), np.random.random()
+    print(err(jacobian(lambda u, x: a * u, y, t), a))
+    print(err(jacobian(lambda u, x: a * u * u, y, t), 2 * a * y))
+    print(err(jacobian(lambda u, x: x * a * u, y, t), t * a))
+    print(err(jacobian(lambda u, x: np.exp(x * u), y, t), t * np.exp(t * y)))
+
+    a, y, t = np.random.random((2, 2)), np.random.random(2), np.random.random()
+    print(err(jacobian(lambda u, x: np.dot(a, u), y, t), a))
+    print(err(jacobian(lambda u, x: np.dot(a, u * u), y, t), 2 * a * y))
+    print(err(jacobian(lambda u, x: x * np.dot(a, u), y, t), t * a))
+    print(err(jacobian(lambda u, x: np.exp(x * u), y, t), t * np.diagflat(np.exp(t * y))))
+
+
 if __name__ == '__main__':
-    test_phi_1()
+    # test_phi_1()
+    test_jacobian()
