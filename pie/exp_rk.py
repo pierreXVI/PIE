5305ada22a5378cc8a20198178e12d5dcb58a618
Maes Theo <maes.theo89@hotmail.fr>
2019-01-30 15:05:54 +0100

    Début méthode exp_RK

--- pie/exp_rk.py
+++ pie/exp_rk.py
@@ -1,5 +1,6 @@
 import numpy as np
-from . import utils
+import scipy as sp
+import utils 
 
 
 def exp_euler(y0, t, f):
@@ -31,4 +32,23 @@ def exp_rosen(y0, t, f):
         # print(a - np.sin(t[i]) ** 2)
         h = (t[i + 1] - t[i])
         y[i + 1] = y[i] + np.dot(h * utils.phi_1(h * a), f(y[i], t[i]))
+    return y
+
+def exp_RK(y0,t,f,verbose=True, **_ ):
+    try: 
+        n,d=len(t),len(y0)
+        y=np.zeros((n,d))  
+    except TypeError: 
+        n=len(t)
+        y=np.zeros((n,))
+    if verbose is False:
+        count = Counter('', 0)
+    elif verbose is True:
+        count = Counter('Exp Rk', n)
+    else:
+        count = Counter(verbose, n)
+    y[0]=y0
+    a = np.atleast_2d(utils.jacobian(f, y[i], t[i]))
+    h = (t[i + 1] - t[i])
+    y[i + 1] = y[i] + np.dot(sp.linalg.expm(h*a), f(y[i], t[i]))
     return y
