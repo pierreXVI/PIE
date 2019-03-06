645614b9e9ef1cdeb6ba9b3cd21fc0ba7aa0450d
Pierre Seize <pierreseize@gmail.com>
2019-03-06 10:46:41 +0100

    Finishing Animation + tests on PDE

--- pie/plot/animation.py
+++ pie/plot/animation.py
@@ -2,57 +2,72 @@ import matplotlib.pyplot as plt
 
 
 class Animation:
-    def __init__(self, t, x, list_y, list_label=None, list_ls=None, ticks=None, repeat=True):
-        self.speed = 1
-
-        self.x = x
+    def __init__(self, t, x, list_y, list_label=None, list_ls=None, list_lw=None, x_ticks=None, repeat=True, speed=1):
         self.t = t
+        self.x = x
         self.list_y = list_y
         self.list_line = len(list_y) * [None]
         if list_label is None:
             list_label = len(list_y) * [None]
         if list_ls is None:
             list_ls = len(list_y) * ['+-']
+        if list_lw is None:
+            list_lw = len(list_y) * [1]
         self.repeat = repeat
+        self.speed = speed
+        self.run = True
 
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(111)
         self.ax.grid(True)
-        if ticks is not None:
-            self.ax.set_xticks(ticks)
+        if x_ticks is not None:
+            self.ax.set_xticks(x_ticks)
 
         self.i = 0
         self.title = 'n_iter = {{0:0>{width1}}}, t = {{1:0>{width2}.2f}}, dt = {{2:0.2E}}' \
             .format(width1=len(str(len(self.t))), width2=len('{0:0.2f}'.format(self.t[-1])))
         for i in range(len(self.list_y)):
-            self.list_line[i], = self.ax.plot(self.x, list_y[i][0], list_ls[i], label=list_label[i])
+            self.list_line[i], = self.ax.plot(self.x, list_y[i][0], list_ls[i], lw=list_lw[i], label=list_label[i])
 
         self.ax.legend()
+        self.fig.canvas.mpl_connect('key_press_event', self.key_event)
         self.update()
         plt.show()
 
     def update(self):
+        if self.i >= len(self.t):
+            if self.repeat:
+                self.i = 0
+            else:
+                self.i = len(self.t) - 1
+                self.run = False
+
         try:
             dt = self.t[self.i + 1] - self.t[self.i]
         except IndexError:
             dt = 0
+
+        self.fig.suptitle('Speed x {0:0.2f}'.format(self.speed))
         self.ax.set_title(self.title.format(self.i, self.t[self.i], dt))
         for i in range(len(self.list_y)):
             self.list_line[i].set_ydata(self.list_y[i][self.i])
         self.fig.canvas.draw()
 
-        if self.i + 1 >= len(self.t):
-            if self.repeat:
-                self.i = 0
-                self.fig.canvas._tkcanvas.after(int(1E3 / self.speed), self.update)
-                return
-            else:
-                return
+        self.i = int(self.i + max(1, self.speed))
+        if self.run:
+            self.fig.canvas._tkcanvas.after(int(1 / self.speed), self.update)
 
-        t0 = self.t[self.i]
-        while self.t[self.i] - t0 < int(1 / self.speed):
-            self.i += 1
-            if self.i >= len(self.t):
-                self.i -= 1
-                break
-        self.fig.canvas._tkcanvas.after(int(1E3 / self.speed), self.update)
+    def key_event(self, event):
+        if event.key == '+':
+            self.speed += 1
+        if event.key == '-':
+            self.speed = max(self.speed - 1, 1)
+        if event.key == '*':
+            self.speed *= 2
+        if event.key == '/':
+            self.speed /= 2
+
+        if event.key == ' ':
+            self.run = not self.run
+            if self.run:
+                self.update()
--- pie/test/test_pde.py
+++ pie/test/test_pde.py
@@ -1,4 +1,3 @@
-import matplotlib.pyplot as plt
 import numpy as np
 
 import pie
@@ -45,17 +44,23 @@ def solve_burgers(n, x_max, p, diff, dt, t_max, init_cond, spatial_method_burger
     return method, t, y
 
 
-def compare(n, x_max, p, conv, diff, dt, t_max, temporal_method, interval=200, repeat=False):
+def compare(n, x_max, p, conv, diff, dt, t_max, temporal_method, repeat=True, speed=1):
     y0 = pie.test.initial_condition.sine(x_max)
     method, t, y_fd = solve(n=n, x_max=x_max, p=p, conv=conv, diff=diff, dt=dt, t_max=t_max, init_cond=y0,
                             spatial_method=pie.spatial.FiniteDifferenceMethod, temporal_method=temporal_method)
     method, t, y_sd = solve(n=n, x_max=x_max, p=p, conv=conv, diff=diff, dt=dt, t_max=t_max, init_cond=y0,
                             spatial_method=pie.spatial.SpectralDifferenceMethod, temporal_method=temporal_method)
 
-    pie.plot.animation.Animation(t, method.x, [y_fd, y_sd], ['FiniteDifferenceMethod', 'SpectralDifferenceMethod'])
+    sol = [y0((method.x - conv * s) % x_max) * np.exp(-diff * s * ((2 * np.pi / x_max) ** 2)) for s in t]
+    pie.plot.animation.Animation(t, method.x,
+                                 [sol, y_fd, y_sd],
+                                 list_label=['Exact solution', 'FiniteDifferenceMethod', 'SpectralDifferenceMethod'],
+                                 list_ls=['k', '+-', '+-'],
+                                 list_lw=[3, 1, 1],
+                                 repeat=repeat, speed=speed)
 
 
-def compare_burgers(n, x_max, p, diff, dt, t_max, init_cond, temporal_method, interval=200, repeat=False):
+def compare_burgers(n, x_max, p, diff, dt, t_max, init_cond, temporal_method, repeat=True, speed=1):
     method, t, y_fd = solve_burgers(n=n, x_max=x_max, p=p, diff=diff, dt=dt, t_max=t_max,
                                     init_cond=init_cond, temporal_method=temporal_method,
                                     spatial_method_burgers=pie.spatial.burgers.FiniteDifferenceMethodBurgers)
@@ -63,48 +68,14 @@ def compare_burgers(n, x_max, p, diff, dt, t_max, init_cond, temporal_method, in
                                     init_cond=init_cond, temporal_method=temporal_method,
                                     spatial_method_burgers=pie.spatial.burgers.SpectralDifferenceMethodBurgers)
 
-    fig = plt.figure()
-    ax = fig.add_subplot(111)
-    ax.grid(True)
-    ax.set_xticks(method.mesh)
-
-    line_fd, = ax.plot(method.x, len(method.x) * [None], '+-', animated=True, label='FiniteDifferenceMethod')
-    line_sd, = ax.plot(method.x, len(method.x) * [None], '+-', animated=True, label='SpectralDifferenceMethod')
-    ax.legend()
-
-    ax.set_xlim(0, x_max)
-    ax.set_ylim(-1.2, 1.2)
-
-    def update(frame):
-        line_fd.set_ydata(y_fd[frame])
-        line_sd.set_ydata(y_sd[frame])
-        return line_fd, line_sd
-
-    _ = FuncAnimation(fig, update, frames=len(t), blit=True, interval=interval, repeat=repeat)
-    plt.show()
-
-
-def plot(method, t, y, temporal_method_name='temporal_method'):
-    fig = plt.figure()
-    ax1 = fig.add_subplot(111)
-    ax1.grid(True)
-    ax1.set_xlabel(r'$x$')
-    ax1.set_title('t = {0} (n_iter = {1}, dt = {2:0.2E})'.format(t[-1], len(t), t[1] - t[0]))
-    ax1.plot(method.x, y[0], label='t = 0', c='k', lw=3)
-    ax1.plot(method.x, y[-1], label='{0} + {1}'.format(temporal_method_name, method.__class__.__name__))
-    ax1.legend()
-
-    plt.show()
+    pie.plot.animation.Animation(t, method.x,
+                                 [y_fd, y_sd],
+                                 list_label=['FiniteDifferenceMethod', 'SpectralDifferenceMethod'],
+                                 repeat=repeat, speed=speed)
 
 
 if __name__ == '__main__':
-    # plot(*solve(n=50, x_max=1, p=4, conv=1, diff=0, dt=1E-3, t_max=1,
-    #             init_cond=pie.test.initial_condition.sine(1),
-    #             spatial_method=pie.spatial.SpectralDifferenceMethod,
-    #             temporal_method=pie.temporal.rk_2))
-    #
-    # plot(*solve_burgers(n=50, x_max=1, p=4, diff=0, dt=1E-3, t_max=0.1,
-    #                     init_cond=pie.test.initial_condition.sine(1),
-    #                     spatial_method_burgers=pie.spatial.burgers.SpectralDifferenceMethodBurgers,
-    #                     temporal_method=pie.temporal.rk_2))
-    compare(n=50, x_max=1, p=4, conv=1, diff=0, dt=1E-3, t_max=5, temporal_method=pie.temporal.rk_2)
+    # compare(n=50, x_max=1, p=4, conv=1, diff=0.01, dt=1E-4, t_max=2, temporal_method=pie.temporal.rk_2,
+    #         speed=10, repeat=True)
+    compare_burgers(n=50, x_max=1, p=4, diff=0.00, dt=1E-3, t_max=1, init_cond=pie.test.initial_condition.rect(1),
+                    temporal_method=pie.temporal.rk_4, speed=10, repeat=True)
