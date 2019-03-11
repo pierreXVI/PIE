b3eef0ef321e0d161e5aa5a5635d2a94f90c9d0c
Pierre Seize <pierreseize@gmail.com>
2019-03-11 18:16:23 +0100

    Adding title

--- pie/plot/animation.py
+++ pie/plot/animation.py
@@ -25,6 +25,8 @@ class Animation:
     :type list_lw: array_like, optional
     :param x_ticks:
     :type x_ticks: array_like, optional
+    :param title:
+    :type title: str, optional
     :param repeat:
     :type repeat: bool, optional
     :param speed:
@@ -34,6 +36,7 @@ class Animation:
     :ivar array_like x: The position of the solution points
     :ivar array_like list_y: The list of curves to plot, of size (n x ``len(t)`` x ``len(x)``)
     :ivar array_like list_y: The list of plotted lines, of size n
+    :ivar str title: The figure title
     :ivar bool repeat: If True, restart the animation when it ends
     :ivar float speed:
     :ivar bool run: If False, the animation is paused of stopped
@@ -41,10 +44,11 @@ class Animation:
     :ivar matplotlib.axes._subplots.AxesSubplot ax:
     :ivar int i: The index of the current time step
     :ivar str title: Title to be formatted with the current iteration number, time and time step
-
+    :ivar str speed_text: Text plotted to show the animation speed
     """
 
-    def __init__(self, t, x, list_y, list_label=None, list_fmt=None, list_lw=None, x_ticks=None, repeat=True, speed=1):
+    def __init__(self, t, x, list_y, list_label=None, list_fmt=None, list_lw=None, x_ticks=None, title='',
+                 repeat=True, speed=1):
         self.t = t
         self.x = x
         self.list_y = list_y
@@ -55,14 +59,15 @@ class Animation:
             list_fmt = len(list_y) * ['+-']
         if list_lw is None:
             list_lw = len(list_y) * [1]
+        self.title = title
         self.repeat = repeat
         self.speed = speed
         self.run = True
 
         self.fig = plt.figure()
         self.ax = self.fig.add_subplot(111)
-        self.ax.set_xlabel('x', fontsize=16)
-        self.ax.set_ylabel('y', fontsize=16)
+        self.ax.set_xlabel('x', fontsize='xx-large')
+        self.ax.set_ylabel('y', fontsize='xx-large')
         self.ax.grid(True)
         if x_ticks is not None:
             self.ax.set_xticks(x_ticks)
@@ -70,11 +75,15 @@ class Animation:
         self.i = 0
         self.title = 'n_iter = {{0:0>{width1}}}, t = {{1:0>{width2}.2f}}, dt = {{2:0.2E}}' \
             .format(width1=len(str(len(self.t))), width2=len('{0:0.2f}'.format(self.t[-1])))
+        self.speed_text = self.fig.text(0.01, 0.95, 'Speed x {0:0.2f}'.format(self.speed), horizontalalignment='left')
         for i in range(len(self.list_y)):
             self.list_line[i], = self.ax.plot(self.x, list_y[i][0], list_fmt[i], lw=list_lw[i], label=list_label[i])
 
-        self.ax.legend(loc='upper right', ncol=2, fontsize=10)
+        self.ax.legend(loc='upper right', ncol=2, fontsize='x-large')
+        self.fig.suptitle(title, fontsize='xx-large')
+
         self.fig.canvas.mpl_connect('key_press_event', self._key_event)
+
         self._update()
         plt.show()
 
@@ -93,7 +102,7 @@ class Animation:
         except IndexError:
             dt = self.t[1] - self.t[0]
 
-        self.fig.suptitle('Speed x {0:0.2f}'.format(self.speed), x=0.1)
+        self.speed_text.set_text('Speed x {0:0.2f}'.format(self.speed))
         self.ax.set_title(self.title.format(self.i, self.t[self.i], dt), fontsize=16)
         for i in range(len(self.list_y)):
             self.list_line[i].set_ydata(self.list_y[i][self.i])
