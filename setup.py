43c508bac9a1e5ca195529faf1cfee032a6fe642
Pierre Seize <pierreseize@gmail.com>
2019-03-24 12:11:35 +0100

    Adding args to setup

--- setup.py
+++ setup.py
@@ -3,5 +3,8 @@ from setuptools import setup, find_packages
 setup(
     name='PIE',
     packages=find_packages(),
-    install_requires=['numpy', 'scipy', 'matplotlib']
+    install_requires=['numpy', 'scipy', 'matplotlib'],
+    maintainer='Pierre Seize',
+    maintainer_email='pierre.seize@gmail.com',
+    url='https://github.com/pierreXVI/PIE',
 )
